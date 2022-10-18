794256da93f0ffcffd25420756043927580e9ed6
Nyallu <anyallu2@gmail.com>
2022-10-18 15:35:39 -0700

    Write a function that produces output according to a format.

--- main.h
+++ main.h
@@ -1,33 +1,83 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H_
+#define MAIN_H_
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <unistd.h>
+#include <string.h>
 
-/**
- * struct format - Struct for format
- * @specifiers: Struct format
- * @f: The function associated
- */
-
-typedef struct specifiers
+/* Flags struct */
+typedef struct flags
 {
-	char specifiers;
-	int (*f)(va_list);
-} specifiers_t;
+	int plus;
+	int space;
+	int hash;
+} flag_t;
 
-/*prototypes*/
-int _printf(const char *format, ...);
-int get_function(char s, va_list args);
+/* Modifiers struct */
+typedef struct modifiers
+{
+	int l;
+	int h;
+	int width;
+} mod_t;
+
+/* identifiers struct */
+typedef struct identifiers
+{
+	char symbol;
+	int (*func)(va_list, flag_t *, mod_t *);
+} id;
+
+
+
+/* Write functions*/
 int _putchar(char c);
+int _puts(char *str);
 
-/*Conversion specifiers*/
-int print_char(va_list args);
-int print_string(va_list args);
-int print_digit(va_list args);
-int print_mod(va_list args);
-int print_rev_string(va_list args);
 
-#endif
+/* _printf */
+int _printf(const char *format, ...);
+
+/* Print character functions */
+int _print_char(va_list, flag_t *, mod_t *);
+int _print_str(va_list, flag_t *, mod_t *);
+int _print_specifier(va_list, flag_t *, mod_t *);
+
+/* Print numbers in decimal */
+int  _print_dec(va_list, flag_t *, mod_t *);
+
+/* Print numbers in other bases */
+int _print_binary(va_list, flag_t *, mod_t *);
+int _print_hexa_upper(va_list, flag_t *, mod_t *);
+int _print_hexa_lower(va_list, flag_t *, mod_t *);
+int _print_octal(va_list, flag_t *, mod_t *);
+int _print_unsigned(va_list, flag_t *, mod_t *);
+int _print_address(va_list, flag_t *, mod_t *);
+
+/* Print addresses in hexadecimal format */
+int _print_address(va_list, flag_t *, mod_t *);
+
+/* Print custom */
+int _print_percent(va_list, flag_t *, mod_t *);
+int _print_reverse(va_list, flag_t *, mod_t *);
+int _print_rot13(va_list, flag_t *, mod_t *);
+
+/* Helper functions */
+void print_number(long n);
+int count_digits(long n);
+char *convert(unsigned long num, int base, int lowercase);
+
+/* Function Pointer Generator */
+int (*get_func(char c))(va_list, flag_t *, mod_t *);
+
+/* Sets flags */
+int get_flags(char elem, flag_t *);
+
+/* Sets the modifiers */
+int get_modifier(char elem, mod_t *);
+void set_width(int width, mod_t *);
+
+#endif /* MAIN_H_ */
